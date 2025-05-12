58d0920ffd75e2ce1d442b33ff6e7459c8cca64d
Tucker Beck <tucker.beck@gmail.com>
2025-05-12 11:46:31 -0700

    step 2: use a client to access api endpoints

--- src/typerdrive_tutorial/cli.py
+++ src/typerdrive_tutorial/cli.py
@@ -1,13 +1,28 @@
+from enum import StrEnum, auto
+
+import httpx
 import typer
 from typerdrive import terminal_message
+
+
+class Endpoint(StrEnum):
+    unsecured = auto()
+    secured = auto()
 
 
 app = typer.Typer()
 
 
 @app.command()
-def access():
-    terminal_message("Processing access command")
+def access(endpoint: Endpoint):
+    response = httpx.get(f"http://localhost:8000/{endpoint}")
+    response.raise_for_status()
+    message = response.json()["message"]
+    terminal_message(
+        message,
+        subject="Successfully connected to API",
+        footer=f"Status Code: {response.status_code}",
+    )
 
 
 @app.command()
